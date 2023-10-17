465a1a71308d9fd83f5cd759fb990f2d538a4fda
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2023-10-17 21:39:04 +0200

    little fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
+import React from "react";
 
 import "./globals.css";
 import ThemeProvider from "@/context/ThemeProvider";
