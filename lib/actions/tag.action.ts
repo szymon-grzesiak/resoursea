8d9cf61a528c09e5e1272d4127bbb460413eae6c
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2023-11-03 13:31:55 +0100

    changes

---
+++ lib/actions/tag.action.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { GetTopInteractedTagsParams } from "./shared.types";
+import { connectToDatabase } from "../mongoose";
+import User from "@/database/user.model";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId, limit = 3 } = params;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Find interactions for the user and group by tags ...
+    // Interaction...
+    return ['tag1', 'tag2', 'tag3'];
+
+  } catch (error) {
+    console.log(error);
+    throw Error;
+  }
+}
