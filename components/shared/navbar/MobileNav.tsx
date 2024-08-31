7092b1d60478807b96ae447ec15bbf638a18e9c1
Szymon Stanisław Grzesiak <szymongrzesiak.pl@gmail.com>
2024-09-01 00:44:29 +0200

    Fixes

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -12,6 +12,7 @@ import { SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
+import LogoNew from '../../../public/assets/images/logo-new.png';
 
 const NavContent = () => {
   const pathname = usePathname();
@@ -67,13 +68,13 @@ const MobileNav = () => {
       >
         <Link href="/" className="flex items-center gap-1">
           <Image
-            src="/assets/images/site-logo.svg"
-            width={23}
-            height={23}
-            alt="DevFlow"
+            src={LogoNew}
+            width={65}
+            height={65}
+            alt="Resoursea"
           />
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
-            Dev <span className="text-primary-500">Overflow</span>
+           <span className="text-primary-500">Resoursea</span>
           </p>
         </Link>
         <div>
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -8,16 +8,16 @@ import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 border-b dark:border-none shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 border-b shadow-light-300 dark:border-none dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/logo-new.png"
           width={80}
           height={80}
-          alt="DevFlow"
+          alt="Resoursea"
         />
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          <span className="text-slate-700 dark:text-primary-500 [text-shadow:_1px_1px_1px_rgb(0_0_0_/_70%)]">Resoursea</span>
+          <span className="text-slate-700 [text-shadow:_1px_1px_1px_rgb(0_0_0_/_70%)] dark:text-primary-500">Resoursea</span>
         </p>
       </Link>
       <GlobalSearch />
